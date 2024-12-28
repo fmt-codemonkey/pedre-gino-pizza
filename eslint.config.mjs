653def47bed103f887444c62a06b91698b2214a0
fmt-codemonkey <fmt.codemonkey@gmail.com>
2024-12-27 23:03:10 -0500

    Backend folder added

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,20 @@
 import js from "@eslint/js";
 import globals from "globals";
+import reactPlugin from "eslint-plugin-react";
 import prettier from "eslint-config-prettier";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   js.configs.recommended,
+  {
+    ...reactPlugin.configs.flat.recommended,
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+  reactPlugin.configs.flat["jsx-runtime"],
   {
     files: ["**/*.js", "**/*.jsx", "**/*.ts", "**/*.tsx"],
     languageOptions: {
@@ -14,6 +24,10 @@ export default [
           jsx: true,
         },
       },
+    },
+    rules: {
+      "react/no-unescaped-entities": "off", // ' -> &apos; (if 'on' we have to do like this)
+      "react/prop-types": "off",
     },
   },
   prettier,
--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
-import React from "react";
 import { createRoot } from "react-dom/client";
+import Pizza from "./components/Pizza";
 
 const App = () => {
-  return <h1>React App</h1>;
+  return (
+    <div>
+      <h1>Padre Gino's - Order Now</h1>
+      <Pizza name="Pepperoni" description="Pepperoni, Cheese and Salami" />
+      <Pizza name="Hawaiian" description="Pineapple, Cheese and Veggies" />
+      <Pizza name="Americano" description="Half shot coffee and milk" />
+    </div>
+  );
 };
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(React.createElement(App));
+root.render(<App />);
